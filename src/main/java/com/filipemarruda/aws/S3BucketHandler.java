b4e596827fb42f454e789679ef082d636f05b3ba
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-08-21 01:11:54 -0300

    refactor S3bucketHandler, excluding useless singletons

--- src/main/java/com/filipemarruda/aws/S3BucketHandler.java
+++ src/main/java/com/filipemarruda/aws/S3BucketHandler.java
@@ -18,25 +18,29 @@ import com.amazonaws.services.s3.transfer.Upload;
 
 public class S3BucketHandler {
 
-	private TransferManager tm;
-	private AmazonS3 s3;
+	private final TransferManager tm;
+	private final AmazonS3 s3;
 
-	private AWSCredentials credentials = null;
-	private String accessKey;
-	private String secretKey;
+	private final AWSCredentials credentials;
+	private final String accessKey;
+	private final String secretKey;
 	
 	public S3BucketHandler(final String accessKey, final String secretKey){
 		
 		this.accessKey = accessKey;
 		this.secretKey = secretKey;
 		this.credentials = new BasicAWSCredentials(accessKey, secretKey);
+		this.tm = new TransferManager(credentials);
+        this.s3 = tm.getAmazonS3Client();
+        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+        this.s3.setRegion(usWest2);
 		
 	}
 	
 	public String putObject(final String bucket, final File file) throws AmazonServiceException, AmazonClientException, InterruptedException{
 		
 		final String key = UUID.randomUUID().toString();
-		Upload upload = getTm().upload(new PutObjectRequest(bucket, key, file));
+		Upload upload = this.tm.upload(new PutObjectRequest(bucket, key, file));
 		upload.waitForCompletion();
 		return key;
 		
@@ -44,36 +48,12 @@ public class S3BucketHandler {
 	
 	public S3Object getObject(final String bucket, final String key){
 		
-		return getS3().getObject(new GetObjectRequest(bucket, key));
+		return this.s3.getObject(new GetObjectRequest(bucket, key));
 		
 	}
 
 	public void deleteObject(final String bucket, final String key){
-		getS3().deleteObject(bucket, key);
-	}
-
-	private TransferManager getTm() {
-		
-		if(tm == null){
-			
-			this.tm = new TransferManager(credentials);
-	        this.s3 = tm.getAmazonS3Client();
-	        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-	        this.s3.setRegion(usWest2);
-	        
-		}
-		
-		return tm;
-	}
-	
-	private AmazonS3 getS3(){
-		
-		if(this.s3 == null){
-			this.s3 = getTm().getAmazonS3Client();
-		}
-		
-		return this.s3;
-		
+		this.s3.deleteObject(bucket, key);
 	}
 	
 	public String getAccessKey() {
