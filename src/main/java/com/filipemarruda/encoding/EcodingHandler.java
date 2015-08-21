a5adf441c78f4628e3106839f1e4708a73ecd707
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-08-21 02:27:00 -0300

    added getMediaInfo method

--- src/main/java/com/filipemarruda/encoding/EcodingHandler.java
+++ src/main/java/com/filipemarruda/encoding/EcodingHandler.java
@@ -1,13 +1,36 @@
 package com.filipemarruda.encoding;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import com.filipemarruda.bundle.Properties;
+import com.filipemarruda.http.HttpConnector;
+
 public class EcodingHandler {
 	
+	private final String encodingEndpoint;
 	private final String userId;
 	private final String userKey;
 	
-	public EcodingHandler(final String userId, final String userKey){
+	public EcodingHandler(final String encodingEndpoint, final String userId, final String userKey){
+		this.encodingEndpoint = encodingEndpoint;
 		this.userId = userId;
 		this.userKey = userKey;
+	}
+	
+	public String getMediaInfo(final String mediaId) throws IOException{
+		
+		String xml = String.format(
+			Properties.getString("EncodingGetMediaInfoXML"),
+			userId,
+			userKey,
+			mediaId
+		);
+			
+		String payload = "xml=" + URLEncoder.encode(xml, "UTF8");
+		
+		return HttpConnector.simplePost(encodingEndpoint, payload);
+		
 	}
 	
 }
--- src/test/java/com/filipemarruda/encoding/EncodingHandlerTest.java
+++ src/test/java/com/filipemarruda/encoding/EncodingHandlerTest.java
@@ -1,11 +1,37 @@
 package com.filipemarruda.encoding;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+import com.filipemarruda.bundle.Properties;
+
 public class EncodingHandlerTest {
+
+	@Test(expected=java.io.IOException.class)
+	public void getMediaInfo_1() throws Exception{
+		
+		final String mediaId = "";
+		EcodingHandler eH = new EcodingHandler(
+				"",
+				"",
+				"");
+		eH.getMediaInfo(mediaId);
+		
+	}
 	
 	@Test
-	public void getMediaInfo(){
+	public void getMediaInfo_2() throws Exception{
+		
+		final String mediaId = "";
+		EcodingHandler eH = new EcodingHandler(
+				Properties.getString("EncodingEndpoint"),
+				Properties.getString("EncodingUserId"),
+				Properties.getString("EncodingUserKey"));
+		
+		String response = eH.getMediaInfo(mediaId);
+		
+		assertTrue(response.contains("error"));
 		
 	}
 	
