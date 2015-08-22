1d5b73b23f040615486fa64b4fbaae5c3030e67c
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-08-22 15:13:28 -0300

    added XML response parser methods

--- src/main/java/com/filipemarruda/xml/XMLParser.java
+++ src/main/java/com/filipemarruda/xml/XMLParser.java
@@ -2,6 +2,8 @@ package com.filipemarruda.xml;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,24 +20,44 @@ import org.xml.sax.SAXException;
 
 public class XMLParser {
 
-	
-	private XMLParser(){
-		/*Private Constructor will prevent 
-	       * the instantiation of this class directly*/
+	private XMLParser() {
+		/*
+		 * Private Constructor will prevent the instantiation of this class
+		 * directly
+		 */
 	};
+
+	public static String getMediaId(final String xml)
+			throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
+		return getStringFromXML("//mediaid", xml);
+	}
 	
-	public static String getStringFromXML(final String xpath, final String xml) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException{
+	public static String getMediaFormatFromResponse(final String xml)
+			throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
 		
+		final Pattern pattern = Pattern.compile("<response>(.*)</response>");
+		final Matcher matcher = pattern.matcher(xml);
+		String response = null;
+		
+		while (matcher.find()) {
+			response = matcher.group(1);
+	    }
+		
+		return response;
+	}
+
+	private static String getStringFromXML(final String xpath, final String xml)
+			throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+
 		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		final DocumentBuilder builder = factory.newDocumentBuilder();
 		final Document document = builder.parse(new InputSource(new StringReader(xml)));
 		final XPathFactory xPathfactory = XPathFactory.newInstance();
 		final XPath xpathObj = xPathfactory.newXPath();
 		final XPathExpression expr = xpathObj.compile(xpath);
-		
+
 		return expr.evaluate(document, XPathConstants.STRING).toString();
-		
+
 	}
-	
-	
+
 }
--- src/test/java/com/filipemarruda/xml/XMLParserTest.java
+++ src/test/java/com/filipemarruda/xml/XMLParserTest.java
@@ -1,32 +1,32 @@
 package com.filipemarruda.xml;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class XMLParserTest {
 
 	@Test
-	public void getStringFromXML_1() throws Exception{
+	public void getMediaId() throws Exception{
 		
-		String result = XMLParser.getStringFromXML("//userid", createMockXML());
-		assertEquals(result , "userId");
-		
-	}
-	
-	@Test
-	public void getStringFromXML_2() throws Exception{
-		
-		String result = XMLParser.getStringFromXML("//bla", createMockXML());
-		assertEquals(result, "");
+		String result = XMLParser.getMediaId(createMockXML());
+		assertEquals(result , "mediaId");
 		
 	}
 	
 	@Test(expected=org.xml.sax.SAXParseException.class)
-	public void getStringFromXML_3() throws Exception{
+	public void getMediaId_2() throws Exception{
 		
-		String result = XMLParser.getStringFromXML("//userid", createFailMockXML());
-		assertEquals(result , "userId");
+		String result = XMLParser.getMediaId(createFailMockXML());
+		assertEquals(result , "mediaId");
+		
+	}
+	
+	@Test
+	public void getMediaFormatFromResponse() throws Exception{
+		
+		String result = XMLParser.getMediaFormatFromResponse(createResponseMockXML());
+		assertNotNull(result);
 		
 	}
 	
@@ -36,5 +36,9 @@ public class XMLParserTest {
 	
 	private String createFailMockXML(){
 		return "<?xml version='1.0'?><query><userid>userId</userid><userkey>userKey</userkey><action>GetMediaInfo</action><mediaid>mediaId</mediaid></query";
+	}
+	
+	private String createResponseMockXML(){
+		return "<?xml version='1.0'?><response><bitrate>2050k</bitrate><duration>7.58</duration><audio_bitrate>64.0k</audio_bitrate><audio_duration>7.522</audio_duration><video_duration>7.522</video_duration><video_codec>wmv2 (WMV2 / 0x32564D57)</video_codec><size>1920x1080</size><audio_codec>wmav2</audio_codec><audio_sample_rate>44100</audio_sample_rate><audio_channels>2</audio_channels><frame_rate>30</frame_rate><format>windows media</format></response>";
 	}
 }
