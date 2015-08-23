730397669d4ef7ab34b29b9d201b7dda8b470002
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-08-23 19:54:48 -0300

    removing tests http call

--- src/test/java/com/filipemarruda/http/HttpConnectorTest.java
+++ src/test/java/com/filipemarruda/http/HttpConnectorTest.java
@@ -1,15 +1,7 @@
 package com.filipemarruda.http;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import org.junit.Before;
 import org.junit.Test;
-
-import com.filipemarruda.bundle.Properties;
 
 public class HttpConnectorTest {
 	
@@ -23,34 +15,6 @@ public class HttpConnectorTest {
 	@Test(expected=java.io.IOException.class)
 	public void simplePost_1() throws Exception{
 		httpConnector.simplePost("", "");
-	}
-	
-	@Test
-	public void simplePost_2() throws Exception{
-		
-		final String response = httpConnector.simplePost(Properties.getString("EncodingEndpoint"), "");
-		assertTrue(response.contains("error"));
-		
-	}
-	
-	@Test
-	public void simplePost_3() throws Exception{
-		
-		final String response = httpConnector.simplePost(Properties.getString("EncodingEndpoint"), mockGetInfoPayload());
-		assertFalse(response.contains("error"));
-		
-	}
-	
-	private String mockGetInfoPayload() throws UnsupportedEncodingException{
-		
-		final String xml = String.format(
-			Properties.getString("EncodingGetMediaInfoXML"),
-			Properties.getString("EncodingUserId"),
-			Properties.getString("EncodingUserKey"),
-			"42282952"
-		);
-		
-		return "xml=" + URLEncoder.encode(xml, "UTF8");
 	}
 	
 }
