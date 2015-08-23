d1a463c3e80bf584ad477e52ea7dc4e7acc5a7d5
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-08-23 19:34:04 -0300

    changing workflow to an async approach

--- src/main/java/com/filipemarruda/encoding/MediaWorkflow.java
+++ src/main/java/com/filipemarruda/encoding/MediaWorkflow.java
@@ -10,25 +10,52 @@ import org.xml.sax.SAXException;
 import com.filipemarruda.xml.XMLParser;
 
 public class MediaWorkflow {
-	
+
 	private EncodingHandler encodingHandler;
 
-	public MediaWorkflow(final String encodingEndpoint, final String userId, final String userKey){
+	public MediaWorkflow(final String encodingEndpoint, final String userId, final String userKey) {
 		encodingHandler = new EncodingHandler(encodingEndpoint, userId, userKey);
 	}
-	
-	public String start(final String source, final String destination) throws IOException, XPathExpressionException, ParserConfigurationException, SAXException{
-		
-		final String addMediaBenchmarkResponse =  encodingHandler.addMediaBenchmark(source, destination);
+
+	public String start(final String source, final String destination)
+			throws IOException, XPathExpressionException, ParserConfigurationException, SAXException, InterruptedException {
+
+		final String addMediaBenchmarkResponse = encodingHandler.addMediaBenchmark(source, destination);
 		final String mediaId = XMLParser.getMediaId(addMediaBenchmarkResponse);
+
+		return mediaId;
+
+	}
+
+	public String getStatus(final String mediaId)
+			throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, InterruptedException {
+
+		final String getStatusResponse = encodingHandler.getStatus(mediaId);
+		return XMLParser.getMediaStatus(getStatusResponse);
+
+	}
+
+	public String getDestination(final String mediaId)
+			throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, InterruptedException {
+
+		final String mediaInfoResponse = encodingHandler.getStatus(mediaId);
+		String destination = XMLParser.getDestination(mediaInfoResponse);
+		destination = "http://" + destination.substring(destination.lastIndexOf("@")+1);
+		
+		return destination;
+
+	}
+
+	public String process(final String mediaId)
+			throws IOException, XPathExpressionException, ParserConfigurationException, SAXException, InterruptedException {
+
 		final String mediaInfoResponse = encodingHandler.getMediaInfo(mediaId);
 		final String format = XMLParser.getMediaFormatFromResponse(mediaInfoResponse);
-		encodingHandler.processMedia(mediaId, format);
-		
-		return mediaId;
-		
+		final String processMediaResponse = encodingHandler.processMedia(mediaId, format);
+		return XMLParser.getProcessMediaMessage(processMediaResponse);
+
 	}
-	
+
 	protected EncodingHandler getEncodingHandler() {
 		return encodingHandler;
 	}
@@ -37,5 +64,5 @@ public class MediaWorkflow {
 	protected void setEncodingHandler(EncodingHandler encodingHandler) {
 		this.encodingHandler = encodingHandler;
 	}
-	
+
 }
--- src/test/java/com/filipemarruda/encoding/MediaWorkflowTest.java
+++ src/test/java/com/filipemarruda/encoding/MediaWorkflowTest.java
@@ -1,5 +1,6 @@
 package com.filipemarruda.encoding;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,7 +30,7 @@ public class MediaWorkflowTest {
 	}
 	
 	@Test
-	public void start() throws UnsupportedEncodingException, IOException, XPathExpressionException, ParserConfigurationException, SAXException{
+	public void start() throws UnsupportedEncodingException, IOException, XPathExpressionException, ParserConfigurationException, SAXException, InterruptedException{
 		
 		final String source = createSouceMock();
 		final String destination = createDestinationMock();
@@ -37,6 +38,18 @@ public class MediaWorkflowTest {
 		when(encodingHandler.addMediaBenchmark(source, destination))
 			.thenReturn("<?xml version='1.0'?><response><message>Added</message><MediaID>42348081</MediaID></response>");
 		
+		final MediaWorkflow mediaWorkflow = createMediaWorkflowMock();
+		// injecting mock
+		mediaWorkflow.setEncodingHandler(encodingHandler);
+		final String mediaId = mediaWorkflow.start(source, destination);
+		assertEquals(mediaId, 42348081);
+		
+	}
+	
+	@Test
+	public void process() throws UnsupportedEncodingException, IOException, XPathExpressionException, ParserConfigurationException, SAXException, InterruptedException{
+		
+		// setup mocks
 		when(encodingHandler.getMediaInfo("42348081"))
 			.thenReturn("<response><bitrate>2050k</bitrate><duration>7.58</duration><audio_bitrate>64.0k</audio_bitrate><audio_duration>7.522</audio_duration><video_duration>7.522</video_duration><video_codec>wmv2 (WMV2 / 0x32564D57)</video_codec><size>1920x1080</size><audio_codec>wmav2</audio_codec><audio_sample_rate>44100</audio_sample_rate><audio_channels>2</audio_channels><frame_rate>30</frame_rate><format>windows media</format></response>");
 
@@ -47,7 +60,38 @@ public class MediaWorkflowTest {
 		final MediaWorkflow mediaWorkflow = createMediaWorkflowMock();
 		// injecting mock
 		mediaWorkflow.setEncodingHandler(encodingHandler);
-		mediaWorkflow.start(source, destination);
+		final String processMediaMessage = mediaWorkflow.process("42348081");
+		assertEquals(processMediaMessage, "Success");
+		
+	}
+	
+	@Test
+	public void getStatus() throws UnsupportedEncodingException, IOException, XPathExpressionException, ParserConfigurationException, SAXException, InterruptedException{
+		
+		// setup mocks
+		when(encodingHandler.getStatus("42348081"))
+			.thenReturn("<response><id>42348081</id><userid>49677</userid><sourcefile>SourceFile</sourcefile><status>Processing</status><format><id>136610241</id><status>Processing</status><output>mp4</output><destination>Destination</destination></format><queue_time>0</queue_time></response>");
+
+		final MediaWorkflow mediaWorkflow = createMediaWorkflowMock();
+		// injecting mock
+		mediaWorkflow.setEncodingHandler(encodingHandler);
+		final String status = mediaWorkflow.getStatus("42348081");
+		assertEquals(status, "Processing");
+		
+	}
+	
+	@Test
+	public void getDestination() throws UnsupportedEncodingException, IOException, XPathExpressionException, ParserConfigurationException, SAXException, InterruptedException{
+		
+		// setup mocks
+		when(encodingHandler.getStatus("42348081"))
+			.thenReturn("<response><id>42348081</id><userid>49677</userid><sourcefile>SourceFile</sourcefile><status>Processing</status><format><id>136610241</id><status>Processing</status><output>mp4</output><destination>Destination</destination></format><queue_time>0</queue_time></response>");
+
+		final MediaWorkflow mediaWorkflow = createMediaWorkflowMock();
+		// injecting mock
+		mediaWorkflow.setEncodingHandler(encodingHandler);
+		final String destination = mediaWorkflow.getDestination("42348081");
+		assertEquals(destination, "Destination");
 		
 	}
 	
